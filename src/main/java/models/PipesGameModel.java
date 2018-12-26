a0db559153aa61adc800135a8608ec21f1b2041b
Meytal Kozlovsky <meytalkozlovsky@gmail.com>
2018-12-26 17:49:14 +0200

    Added connect and disconnect
    Signed-off-by: Meytal Kozlovsky <meytalkozlovsky@gmail.com>

--- src/main/java/models/PipesGameModel.java
+++ src/main/java/models/PipesGameModel.java
@@ -5,10 +5,26 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ListProperty;
 
 import java.awt.*;
+import java.io.IOException;
+import java.net.Socket;
 
 public class PipesGameModel {
+
+    private Socket serverSocket;
     public BooleanProperty isGoal;
     public IntegerProperty numberOfSteps;
     public ListProperty<char[]> gameBoard;
 
+
+    public void connect(String ip, String port) throws IOException {
+        int portNum = Integer.parseInt(port);
+        this.serverSocket = new Socket(ip, portNum);
+        System.out.println("Connected to server");
+    }
+
+    public void disconnect() throws IOException{
+        if (this.serverSocket != null) {
+            this.serverSocket.close();
+        }
+    }
 }
